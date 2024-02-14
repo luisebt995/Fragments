d8b7c84eadcfb12218e7fb23a581e41036facee9
Luis Enrique <luisebt995@gmail.com>
2024-02-14 18:54:17 +0100

    Show fragment in activity_main through a FragmentContainerView and supportFragmentManager.commit{}.

--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.commit {
+            add<FirstFragment>(R.id.fragCon1)
+        }
     }
 }
